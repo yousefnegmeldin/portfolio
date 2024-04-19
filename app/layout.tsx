a641d7282561d8840304660a565ff56b71342c08
yousefnegmeldin <yousefnegm@gmail.com>
2024-04-19 22:00:30 +0200

    added vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./ui/Navbar";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +21,7 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased max-w-3xl mb-40 flex flex-col mx-4 mt-8 lg:mx-auto`}>
             <Navbar />
             {children}
+            <SpeedInsights />
       </body>
     </html>
   );
