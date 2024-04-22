6c36cd696a23bb76425db0bc074119dcb8d355eb
yousefnegmeldin <yousefnegm@gmail.com>
2024-04-23 01:13:53 +0200

    Revert "Merge branch 'work'"
    This reverts commit e7dac3481795fb1472ad52d2cf3225ef5463643e, reversing
    changes made to 6bf920e5b6d267ff947363cce07c28a79e73f5da.

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import Showcase from '../ui/Showcase'
-import opengraph from '../opengraph-image.jpeg'
+
 const page = () => {
   return (
     <main className="flex flex-col gap-8 mt-16">
       <h2 className="text-2xl font-bold">my work 💻</h2>
-      <p>My work ranges from web development to university projects using a wide variety of programming languages,
-        but I mainly focus on web development as it is my passion and hobby.</p>
-      <Showcase imageSrc={opengraph} text="yo" />
+      
       
     </main>
   )
