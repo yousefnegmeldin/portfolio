a7594a8011d38878c4f19824cd2df677887d6071
yousefnegmeldin <yousefnegm@gmail.com>
2024-06-19 22:21:15 +0300

    debugging

--- app/api/pageview/[id]/route.js
+++ app/api/pageview/[id]/route.js
@@ -3,13 +3,17 @@ import path from "path";
 import { NextResponse } from "next/server";
 
 export async function GET(req, { params }) {
-  const id = params.id;
-  const counterFilePath = path.resolve(`counters/blog${id}.txt`);
-  let count = 0;
-  if (fs.existsSync(counterFilePath)) {
-    count = parseInt(fs.readFileSync(counterFilePath, "utf8")) || 0;
+  try {
+    const id = params.id;
+    const counterFilePath = path.resolve(`counters/blog${id}.txt`);
+    let count = 0;
+    if (fs.existsSync(counterFilePath)) {
+      count = parseInt(fs.readFileSync(counterFilePath, "utf8")) || 0;
+    }
+    count += 1;
+    fs.writeFileSync(counterFilePath, count.toString());
+    return NextResponse.json({ count });
+  } catch (e) {
+    return NextResponse.json({ e });
   }
-  count += 1;
-  fs.writeFileSync(counterFilePath, count.toString());
-  return NextResponse.json({ count });
 }
