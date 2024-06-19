c72a93ea1959c5cb770ef115ea7244067ee704a4
yousefnegmeldin <yousefnegm@gmail.com>
2024-06-19 23:11:36 +0300

    removed counter

--- app/blog/first/page.tsx
+++ app/blog/first/page.tsx
@@ -4,23 +4,9 @@ import Content from './Content.mdx'
 
 
 const Page = () => {
-    const [views, setViews] = useState(0);
-    useEffect(() => {
-
-        const fetchPageViews = async ()=>{
-          const res = await fetch('/api/pageview/2');
-          const data = await res.json();
-          console.log(data);
-          setViews(data.count);
-        }
-    
-        fetchPageViews();
-      }, []);
   return (
     <div className='prose lg:prose-xl prose-neutral prose-invert prose-code:prose-neutral prose-pre:prose-neutral'>
-        <p className='sm:mt-16'>Views: {views}</p>
-        <Content />
-        
+        <Content />       
     </div>
   )
 }
