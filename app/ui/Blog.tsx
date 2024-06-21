6d31883f419a1a76a84c6b26b90f21361689351c
yousefnegmeldin <yousefnegm@gmail.com>
2024-06-21 17:48:00 +0300

    refactored blog

---
+++ app/ui/Blog.tsx
@@ -0,0 +1,25 @@
+import React,{FC} from 'react'
+import Link from 'next/link'
+import Image, { StaticImageData } from 'next/image'
+type BlogProps = {
+    title:string,
+    date:string,
+    alt:string,
+    imageSrc:StaticImageData,
+}
+
+const Blog:FC<BlogProps> = ({title,date,alt,imageSrc}) => {
+  return (
+    <Link href="/blog/javascript_promises" className='w-[calc(50%-0.5rem)]'>
+        <div className='rounded-2xl'>
+          <Image src={imageSrc} alt={alt} className='rounded-tl-2xl rounded-tr-2xl' />
+          <div className='w-full bg-gray-400 flex flex-col gap-4 bg-zinc-900 p-4 rounded-bl-2xl rounded-br-2xl'>
+            <h1 className='text-md font-bold lg:text-2xl'> {title} </h1>
+            <p className='text-neutral-300'>{date}</p>
+          </div>
+        </div>
+        </Link>
+  )
+}
+
+export default Blog
