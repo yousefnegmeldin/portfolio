3e7ecc37a9b4c2b8acf8a6764615b3fe9bcb86d7
yousefnegmeldin <yousefnegm@gmail.com>
2024-06-23 03:43:49 +0300

    added opengraph for blog

--- app/blog/2024_summer/page.tsx
+++ app/blog/2024_summer/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React,{useState,useEffect} from 'react'
+import Head from 'next/head';
 import Content from './Content.mdx'
 // import hljs from 'highlight.js/lib/core';
 // import go from 'highlight.js/lib/languages/go';
@@ -10,9 +11,20 @@ import Content from './Content.mdx'
 const Page = () => {
 
   return (
+    <>
+    <Head>
+        <title>How I&apos;ll be spending my 2024 summer</title>
+        <meta property="og:title" content="How I'll be spending my 2024 summer" />
+        <meta property="og:description" content="2024 summer goals" />
+        <meta property="og:image" content="/blogs/golang_summer.jpg" />
+        <meta property="og:url" content="https://www.yousefnegm.dev/blog/2024_summer" />
+        <meta property="og:type" content="article" />
+        {/* Add additional meta tags as needed */}
+      </Head>
     <div className='prose lg:prose-xl prose-neutral prose-invert prose-code:prose-neutral prose-pre:prose-neutral'>
         <Content />       
     </div>
+    </>
   )
 }
 
