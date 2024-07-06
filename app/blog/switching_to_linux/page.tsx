8181c919236242226a39d3b45793b7b14b4ce478
yousefnegmeldin <yousefnegm@gmail.com>
2024-07-06 19:11:28 +0300

    fixed opengraph

--- app/blog/switching_to_linux/page.tsx
+++ app/blog/switching_to_linux/page.tsx
@@ -1,29 +1,27 @@
-
-import React,{useState,useEffect} from 'react'
-import { Metadata } from 'next';
-import Head from 'next/head';
-import Content from './Content.mdx'
+import React, { useState, useEffect } from "react";
+import { Metadata } from "next";
+import Head from "next/head";
+import Content from "./Content.mdx";
 // import hljs from 'highlight.js/lib/core';
 // import go from 'highlight.js/lib/languages/go';
 // import "cdn.com/atom-one-dark.min.css"
 
 export const metadata: Metadata = {
-  title: "How I'll be spending my 2024 summer",
-  description: "2024 summer goals",
+  title: "My experience switching to linux",
+  description: "switching to linux from windows",
   openGraph: {
-    title: "How I'll be spending my 2024 summer",
-    description: "2024 summer goals",
-    url: "https://www.yousefnegm.dev/blog/2024_summer",
-    images: '/blog/2024_summer/opengraph-image.jpg',
+    title: "My experience switching to linux",
+    description: "switching to linux from windows",
+    url: "https://www.yousefnegm.dev/blog/switching_to_linux",
+    images: "/blog/switching_to_linux/opengraph-image.jpg",
     type: "article",
   },
 };
 
 const Page = () => {
-  
   return (
     <>
-    {/* <Head key={1}>
+      {/* <Head key={1}>
         <title>How I&apos;ll be spending my 2024 summer</title>
         <meta property="og:title" content="How I'll be spending my 2024 summer" />
         <meta property="og:description" content="2024 summer goals" />
@@ -31,11 +29,11 @@ const Page = () => {
         <meta property="og:url" content="https://www.yousefnegm.dev/blog/2024_summer" />
         <meta property="og:type" content="article" />
       </Head> */}
-    <div className='prose lg:prose-xl prose-neutral prose-invert prose-code:prose-neutral prose-pre:prose-neutral'>
-        <Content />       
-    </div>
+      <div className="prose lg:prose-xl prose-neutral prose-invert prose-code:prose-neutral prose-pre:prose-neutral">
+        <Content />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
